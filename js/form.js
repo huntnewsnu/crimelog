eeb59a472543042dff89b35821ca3035b48c4d5e
Erica Yee <23279506+ericayee@users.noreply.github.com>
2019-11-15 15:37:15 -0500

    started implementing placeholder javascript functions

--- js/form.js
+++ js/form.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+
+/* global variable to keep track of entries */
+var counter = 1;
+
+/* increments counter */
+function increment(){
+  i += 1;
+}
+
+/* formats previous entry and adds new form group */
+function newEntry (){
+  console.log('hello world')
+}
+
+/* formats final log and outputs chart */
+function finishLog() {
+  console.log('log finished!')
+}
